676a977311aaba827acc60c03e023a3a4d9e33f7
Imran <webimran2021@gmail.com>
2023-10-20 02:33:45 +0600

    about section responsive

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import BrandCard from "../BrandCardPage/BrandCard";
 import { useEffect, useState } from "react";
@@ -17,31 +17,22 @@ const Home = () => {
             <Banner></Banner>
             {/* about section */}
 
-            <div className="flex items-center justify-center gap-28 mt-52">
+            <div className="flex flex-col text-center lg:text-left md:flex-col lg:flex-row items-center justify-center gap-28 mt-52">
                 <div>
-                    <img className="" src="../../../public/image/Beauty Shop.jpg" alt="" />
+                    <img className="w-[45rem] p-5" src="../../../public/image/Beauty Shop.jpg" alt="" />
                 </div>
-                <div className="w-[30rem]">
+                <div className="lg:w-[30rem] p-10 md:p-10 lg:p-0">
                     <h2 className="text-7xl font-bold uppercase">About<br /><span className="text-[#4E7661]">Beauty</span> Shop</h2>
-                    <p>We believe that beauty is not just about makeup; it's about empowerment, self-care, and individuality. We have a passion for bringing out the best in everyone and providing a space where you can express yourself through the art of cosmetics and skincare.</p><br />
-                    <p>Client satisfaction is our top priority. Our team is dedicated to making your beauty shopping experience exceptional, whether you're a makeup novice or an experienced beauty enthusiast.</p>
-                    <div className="flex items-center">
+                    <p className="mt-10">We believe that beauty is not just about makeup; it's about empowerment, self-care, and individuality. We have a passion for bringing out the best in everyone and providing a space where you can express yourself through the art of cosmetics and skincare.<br /><br />
+                    Client satisfaction is our top priority. Our team is dedicated to making your beauty shopping experience exceptional, whether you're a makeup novice or an experienced beauty enthusiast.</p>
+                    <div className="flex flex-col lg:flex-row  items-center justify-between mt-10">
                         <div>
-                            <h4>Message</h4>
-                            <div className="join">
-                                <div>
-                                    <div>
-                                        <input className="input input-bordered join-item" placeholder="Search" />
-                                    </div>
-                                </div>
-                                <div className="indicator">
-                                    <button className="btn join-item">Search</button>
-                                </div>
-                            </div>
+                            <h4 className="font-bold">Email</h4>
+                          <Link className="link link-hover">info@beautyshop.com</Link>
                         </div>
-                        <div>
-                            <h4>Call Now</h4>
-                            <p>1-814-251-9966</p>
+                        <div className="mt-5 lg:mt-0">
+                            <h4 className="font-bold">Call Now</h4>
+                            <Link className="link link-hover">61-251-9966</Link>
                         </div>
                     </div>
                 </div>
@@ -60,7 +51,7 @@ const Home = () => {
 
 
             {/* brand section */}
-            <div className="text-center w-4/5 mx-auto text-black mt-40">
+            <div className="text-center w-4/5 mx-auto text-black mt-52">
                 <h2 className="text-7xl font-bold uppercase text-center my-4 text-black mt-20">
                     Our <span className="text-[#4E7661]">Brand</span>
                 </h2>
