501cea73a30c708e437ed1305c266bd230500c0c
Imran <webimran2021@gmail.com>
2023-10-20 16:29:27 +0600

    connect database

--- src/Pages/AddProductPage/AddProduct.jsx
+++ src/Pages/AddProductPage/AddProduct.jsx
@@ -1,9 +1,44 @@
 const AddProduct = () => {
+  const handleAddProduct = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const image = form.url.value;
+    const brand = form.brand.value;
+    const category = form.category.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const description = form.description.value;
+    const newProduct = {
+      name,
+      image,
+      brand,
+      category,
+      price,
+      rating,
+      description,
+    };
+    console.log(newProduct);
+
+    // send data for server
+    fetch("http://localhost:5000/product", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="hero min-h-screen mt-10 mb-40">
       <div className="hero-content flex-col ">
         <div className="card flex-shrink-0 max-w-screen-xl shadow-2xl bg-[#F4F3F0]">
-          <form className="card-body p-28">
+          <form onSubmit={handleAddProduct} className="card-body p-28">
             <div className="text-center">
               <h1 className="text-5xl font-bold">Add New Product</h1>
               <p className="py-6">
@@ -33,48 +68,14 @@ const AddProduct = () => {
                 <input
                   type="url"
                   name="url"
-                  placeholder="Image"
+                  placeholder="Image url"
                   className="input input-bordered"
                   required
                 />
               </div>
             </div>
 
-            <div className="md:flex mb-5 gap-2">
-              <div className="form-control md:w-1/2">
-                <label className="label">
-                  <span className="label-text font-bold">Brand Name</span>
-                </label>
-                <select class="input input-bordered">
-                  <option disabled selected>
-                    Brand name
-                  </option>
-                  <option>L'Oréal</option>
-                  <option>Estée Lauder</option>
-                  <option>Chanel</option>
-                  <option>Revlon</option>
-                  <option>Dior</option>
-                  <option>Avon</option>
-                </select>
-              </div>
-
-              <div className="form-control md:w-1/2">
-                <label className="label">
-                  <span className="label-text font-bold">Type</span>
-                </label>
-                <select className="input input-bordered">
-                  <option disabled selected>
-                    Category Name
-                  </option>
-                  <option>Mascara</option>
-                  <option>Lotion</option>
-                  <option>Makeup</option>
-                  <option>Hair Color</option>
-                  <option>Perfume</option>
-                  <option>Lipstick</option>
-                </select>
-              </div>
-            </div>
+       
 
             <div className="md:flex mb-5 gap-2">
               <div className="form-control md:w-1/2">
@@ -83,7 +84,7 @@ const AddProduct = () => {
                 </label>
                 <input
                   type="number"
-                  name="number"
+                  name="price"
                   placeholder="Price"
                   className="input input-bordered"
                   required
@@ -96,7 +97,7 @@ const AddProduct = () => {
                 </label>
                 <input
                   type="number"
-                  name="number"
+                  name="rating"
                   placeholder="1 to 5"
                   className="input input-bordered"
                   required
@@ -109,9 +110,84 @@ const AddProduct = () => {
                 <span className="label-text font-bold">Description</span>
               </label>
               <textarea
-                class="textarea textarea-bordered h-24 "
+                className="textarea textarea-bordered h-24 "
                 placeholder="Short description"
+                name="description"
               />
+            </div>
+
+            <div className="flex justify-around">
+              <div className="form-control">
+                <label className="label">
+                  <p className="label-text font-bold">Brand Name</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Loreal" />
+                  <p className="label-text font-bold ml-5">Loreal</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Estee Lauder" />
+                  <p className="label-text font-bold ml-5">Estee Lauder</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Chanel" />
+                  <p className="label-text font-bold ml-5">Chanel</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Revlon" />
+                  <p className="label-text font-bold ml-5">Revlon</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Dior" />
+                  <p className="label-text font-bold ml-5">Dior</p>
+                </label>
+
+                <label className="label">
+                  <input type="radio" name="brand" value="Avon" />
+                  <p className="label-text font-bold ml-5">Avon</p>
+                </label>
+              </div>
+
+              <div className="space-x-3">
+                <label className="label space-x-5">
+                  <p className="label-text font-bold">Type</p>
+                </label>
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Mascara" />
+                  <p className="label-text font-bold">Mascara</p>
+                </label>
+
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Lotion" />
+                  <p className="label-text font-bold">Lotion</p>
+                </label>
+
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Makeup" />
+                  <p className="label-text font-bold">Makeup</p>
+                </label>
+
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Hair Color" />
+                  <p className="label-text font-bold">Hair Color</p>
+                </label>
+
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Perfume" />
+                  <p className="label-text font-bold">Perfume</p>
+                </label>
+
+                <label className="label space-x-5">
+                  <input type="radio" name="category" value="Lipstick" />
+                  <p className="label-text font-bold">Lipstick</p>
+                </label>
+
+              </div>
             </div>
 
             <div className="form-control mt-6">
